43a0cefc70de264338a8b1b999573214ed9d18db
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-10-19 15:23:05 +0200

    Steg 1

---
+++ src/enums/Storaläxan.java
@@ -0,0 +1,15 @@
+package enums;
+
+public class Storaläxan {
+	
+public static void main(String[] args) {
+	System.out.println(fahrenheitToCelsius(50));
+}
+
+	public static double fahrenheitToCelsius(double fahrenheit) { // Steg 1
+	    return ((fahrenheit - 32)* 1.8);
+
+	 }
+	
+	}
+
