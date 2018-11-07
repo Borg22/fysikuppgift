10bbf4753e46b1131ab6da22f1726d0f3a908559
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-11-07 10:39:53 +0100

    Andra och tredja frågan

--- src/enums/Storaläxan.java
+++ src/enums/Storaläxan.java
@@ -6,10 +6,18 @@ public static void main(String[] args) {
 	System.out.println(fahrenheitToCelsius(50));
 }
 
-	public static double fahrenheitToCelsius(double fahrenheit) { // Steg 1
+	public static double fahrenheitToCelsius(double fahrenheit) { // Den här metode gör om fahrenheir till celsius
 	    return (fahrenheit - 32) / 1.8;
 
 	}
+	
+	public static double kelvinToCelsius(double kelvin) { // Den här metoden gör om kelvin till celsius
+		return kelvin -273.15;
+	}
+    public static double fluidPressure(FluidTable fluid, double deep) { // Den här metoden räknar ut trycket i vattnet på ett visst djup
+    	double pressure = fluid.density*G*deep;
+    	reture pressure;
+    }
 	
 	
 	}
