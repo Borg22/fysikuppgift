1970ec2c4813765c8fbc630534c004dab1189aa7
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-11-07 14:36:04 +0100

    Java dokumentation

--- src/enums/Sista.java
+++ src/enums/Sista.java
@@ -4,7 +4,17 @@ public class Sista {
 	
 public static void main(String[] args) {
 	
+	//Hur mycket väger 80dm^3
 	System.out.println("uppgift 1 = " + Storaläxan.volumeToMass(SolidTable.IRON,0.080) + "KG");
+
+	// Hur långt hinner Tonka om han har medelhastigheten 2.8m/s i 60 minuter? 
+	System.out.println("uppgift 2 = " + Storaläxan.svtDistance(2.8, 60 * 60));
 	
+	//Hur mycket krävs det för att värma upp 5 liter vatten?
+	System.out.println("uppgift 3 = "+ Storaläxan.heat(FluidTable.WATER, 0.005, 1));
+	
+	//Hur stort är det totala trycket 100 meter under havsytan?
+	System.out.println("uppgift 4 = " + Storaläxan.pressureUnderWater(100) + );
 }
+
 }
--- src/enums/Storaläxan.java
+++ src/enums/Storaläxan.java
@@ -5,84 +5,182 @@ public class Storaläxan {
 public static void main(String[] args) {
 	System.out.println(fahrenheitToCelsius(50));
 }
-//Steg 1/Den här metode gör om fahrenheir till celsius.
+/**Steg 1/Den här metode gör om fahrenheir till celsius.
+ * 
+ * @param fahrenheit
+ * @return
+ */
 	public static double fahrenheitToCelsius(double fahrenheit) { 
 	    return (fahrenheit - 32) / 1.8;
 	}
-	// Steg 2/Den här metoden gör om kelvin till celsius.
+	/** Steg 2/Den här metoden gör om kelvin till celsius.
+	 * 
+	 * @param kelvin
+	 * @return
+	 */
 	public static double kelvinToCelsius(double kelvin) { 
 		return kelvin -273.15;
 	}
-	// Steg3/Den här metoden räknar ut trycket på ett visst djup.
+	/** Steg3/Den här metoden räknar ut trycket på ett visst djup.
+	 * 
+	 * @param fluid
+	 * @param deep
+	 * @return
+	 */
     public static double fluidPressure(FluidTable fluid, double deep) { 
     	double pressure = fluid.density*9.82*deep;
     	return pressure;
     } 
- // Steg4/Den här metoden räknar ut villket tryck det är på ett visst djup i vatten.
+ /** Steg4/Den här metoden räknar ut villket tryck det är på ett visst djup i vatten.
+  * 
+  * @param deep
+  * @return
+  */
     public static double pressureUnderWater(double deep) { 
     	return deep*9.82*0.998*1E3;
     }
-  //Steg5/En metod som räknar ut kinetisk energi med hjälp av massa och hastighet.
+  /**Steg5/En metod som räknar ut kinetisk energi med hjälp av massa och hastighet.
+   * 
+   * @param mass
+   * @param velocity
+   * @return
+   */
     public static double kineticEnergy(double mass, double velocity) { 
     	return (mass*velocity*velocity)/2;
     }
-  //Steg6/En metod som räknar ut potentiell energi med hjälp av massa och höjd.
+  /**Steg6/En metod som räknar ut potentiell energi med hjälp av massa och höjd.
+   * 
+   * @param mass
+   * @param height
+   * @return
+   */
     public static double potentialEnergy(double mass, double height) { 
     	return mass*height*9.82;
     }
- // Steg7/En metod som räknar ut hur hög hastighet man kommer upp i som man släpper ett föremål från en viss höjd.
+ /** Steg7/En metod som räknar ut hur hög hastighet man kommer upp i som man släpper ett föremål från en viss höjd.
+  * 
+  * @param height
+  * @return
+  */
     public static double fallSpeed(double height) {
     	return Math.sqrt(2*9.82*height);
     }
-    // Steg8/En metod som räknar ut skillnad mellan två givna värden.
+    /** Steg8/En metod som räknar ut skillnad mellan två givna värden.
+     * 
+     * @param first
+     * @param last
+     * @return
+     */
    public static double delta(double first, double last) {
 	   return last - first;		
    }
-// Steg9/En metod som gör om en viss volym av vätska till en massa.
+/**Steg9/En metod som gör om en viss volym av vätska till en massa.
+ * 
+ * @param fluid
+ * @param volume
+ * @return
+ */
    public static double volumeToMass(FluidTable fluid, double volume) {
 	   return volume*fluid.density;
    }
- //Steg10/En metod som gör om en viss volym av gas till en massa.
+ /**
+  * Steg10/En metod som gör om en viss volym av gas till en massa.
+  * @param gas
+  * @param volume
+  * @return
+  */
    public static double volumeToMass(GasTable gas, double volume) { 
 	   return volume*gas.density;
    }
- //Steg11/En metod som gör om en viss volym av materia till en massa.
+ /**Steg11/En metod som gör om en viss volym av materia till en massa.
+  * 
+  * @param solid
+  * @param volume
+  * @return
+  */
    public static double volumeToMass(SolidTable solid, double volume) { 
 	   return volume*solid.density;
    }
-   //Steg12/En metod som räknar ut medelhastigheten med hjälp av sträcka och tid.
+   /**Steg12/En metod som räknar ut medelhastigheten med hjälp av sträcka och tid.
+    * 
+    * @param distance
+    * @param time
+    * @return
+    */
    public static double svtVelocity(double distance, double time) {
 	   return distance/time;
    }
- //Steg13/En metod som räknar ut sträcka med hjälp av hastighet och tid.
+ /**Steg13/En metod som räknar ut sträcka med hjälp av hastighet och tid.
+  * 
+  * @param velocity
+  * @param time
+  * @return
+  */
    public static double svtDistance(double velocity, double time) {
 	   return velocity*time;
    }
- //Steg14/En metod som räknar ut tid för hjälp av sträcka och hastighet.
+ /**Steg14/En metod som räknar ut tid för hjälp av sträcka och hastighet.
+  * 
+  * @param distance
+  * @param velocity
+  * @return
+  */
    public static double svtTime(double distance, double velocity) {
 	   return distance / velocity;
    }
- //Steg15/En metod som räknar ut arbete med hjälp av kraft och sträcka.
+ /**Steg15/En metod som räknar ut arbete med hjälp av kraft och sträcka.
+  * 
+  * @param force
+  * @param distance
+  * @return
+  */
    public static double work(double force, double distance) {
 	   return force*distance;
    }
- //Steg16/En metod som räknar ut effekt med hjälp av arbete och tid.
+ /**Steg16/En metod som räknar ut effekt med hjälp av arbete och tid.
+  * 
+  * @param work
+  * @param time
+  * @return
+  */
    public static double power(double work, double time) { 
 	   return work/time;
    }
- //Steg17/En metod som räknar ut hur mycket energi som krävs för att värma ett visst material ett angivet antal grader.
+ /**Steg17/En metod som räknar ut hur mycket energi som krävs för att värma ett visst material ett angivet antal grader.
+  * 
+  * @param solid
+  * @param mass
+  * @param deltaT
+  * @return
+  */
    public static double heat(SolidTable solid, double mass, double deltaT) {
 	   return mass*solid.heatCapacity*deltaT;
    }
- //Steg18/En metod som räknar ut hur mycket energi som krävs för att värma en viss volym vätska ett angivet antal grader.
-   public static double heat(FluidTable fluid, double volume, double deltaT) {
-	   return volume*fluid.density*fluid.heatCapacity*deltaT;
+ /**Steg18/En metod som räknar ut hur mycket energi som krävs för att värma en viss volym vätska ett angivet antal grader.
+  * 
+  * @param fluid
+  * @param volume
+  * @param deltaT
+  * @return
+  */
+   public static double heat(FluidTable fluid, double mass, double deltaT) {
+	   return mass*fluid.density*fluid.heatCapacity*deltaT;
    }
- //Steg19/En metod som räknar ut hur mycket energi som krävs för att värma en viss volym gas ett angivet antal grader.
+ /**Steg19/En metod som räknar ut hur mycket energi som krävs för att värma en viss volym gas ett angivet antal grader.
+  * 
+  * @param gas
+  * @param volume
+  * @param deltaT
+  * @return
+  */
    public static double heat(GasTable gas, double volume, double deltaT) {
 	   return volume*gas.density*gas.heatCapacity*deltaT;
    }
-// Steg20/En metod som räknar ut hur högt ett föremål med en viss hastighet uppåt kommer.
+/** Steg20/En metod som räknar ut hur högt ett föremål med en viss hastighet uppåt kommer.
+ * 
+ * @param velocity
+ * @return
+ */
    public static double velocityToHeight(double velocity) { 
 	   return Math.pow(velocity, 2)/(2*9.82);
    }
